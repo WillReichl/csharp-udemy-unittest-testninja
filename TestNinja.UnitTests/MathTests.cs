aa0ae7ac9bfdfed241c05febff627a1cc89a770b
Will Reichl <William.Reichl@dcsg.com>
2018-12-18 21:55:52 -0500

    S2L22 - Black Box Testing - Number of Tests Sometimes > Number of Execution Paths

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -15,5 +15,44 @@ namespace TestNinja.UnitTests
 
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void Max_WhenFirstArgumentGreater_ReturnFirstArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(2, 1);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_WhenSecondArgumentGreater_ReturnSecondArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(1, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_WhenArgsAreEqual_ReturnFirstArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(1, 1);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(1));
+        }
     }
 }
