e7451c34d6b102b7e23dcb43e7e5c2f4a3327f19
Will Reichl <William.Reichl@dcsg.com>
2018-12-18 21:46:46 -0500

    S2L20 - Writing a Simple Unit Test

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
