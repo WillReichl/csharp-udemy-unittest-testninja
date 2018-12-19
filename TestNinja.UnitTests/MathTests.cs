21f9febb5373c69f117f126f6dead6bfb0974a4c
Will Reichl <William.Reichl@dcsg.com>
2018-12-19 06:52:12 -0500

    S2L22 - Added SetUp method. Used to initialize same "Arrange" object to a new instance across all test methods. Reduces redundant code in each method.

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -6,52 +6,42 @@ namespace TestNinja.UnitTests
     [TestFixture]
     class MathTests
     {
+        private Math _math;
+
+        // SetUp -- called before each test
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math(); // new instance before each test
+        }
+
+        // TearDown -- called after each test -- often used w/ integration tests, e.g. restore data in database to prior state
+
         [Test]
         public void Add_WhenCalled_ReturnSumOfArguments()
         {
-            var math = new Math();
-
-            var result = math.Add(1, 2);
-
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_WhenFirstArgumentGreater_ReturnFirstArgument()
         {
-            // Arrange
-            var math = new Math();
-
-            // Act
-            var result = math.Max(2, 1);
-
-            // Assert
+            var result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_WhenSecondArgumentGreater_ReturnSecondArgument()
         {
-            // Arrange
-            var math = new Math();
-
-            // Act
-            var result = math.Max(1, 2);
-
-            // Assert
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_WhenArgsAreEqual_ReturnFirstArgument()
         {
-            // Arrange
-            var math = new Math();
-
-            // Act
-            var result = math.Max(1, 1);
-
-            // Assert
+            var result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
