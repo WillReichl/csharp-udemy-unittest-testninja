fa254f42db30be84ad3e713903266a099f2a7105
Will Reichl <William.Reichl@dcsg.com>
2018-12-22 07:51:53 -0500

    S2L23 - Parameterized Tests in NUnit

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -25,24 +25,13 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
-        public void Max_WhenFirstArgumentGreater_ReturnFirstArgument()
+        [TestCase(1,2,2)]
+        [TestCase(2,1,2)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnGreaterArgument (int a, int b, int expectedResult)
         {
-            var result = _math.Max(2, 1);
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_WhenSecondArgumentGreater_ReturnSecondArgument()
-        {
-            var result = _math.Max(1, 2);
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_WhenArgsAreEqual_ReturnFirstArgument()
-        {
-            var result = _math.Max(1, 1);
-            Assert.That(result, Is.EqualTo(1));
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
