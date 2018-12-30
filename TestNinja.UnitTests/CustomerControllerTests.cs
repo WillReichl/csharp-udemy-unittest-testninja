2f54b49fec858de02601a188bdc14165c729b5f8
Will Reichl <William.Reichl@dcsg.com>
2018-12-30 07:58:01 -0500

    S3L31 - Testing Return Type of Methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void SetUp ()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound ()
+        {
+            var result = _customerController.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>()); // NotFound object
+            Assert.That(result, Is.InstanceOf<NotFound>()); // NotFound object or derivative of NotFound
+            Assert.That(result, Is.InstanceOf<ActionResult>()); // example usage
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk ()
+        {
+            var result = _customerController.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>()); // NotFound object
+            Assert.That(result, Is.InstanceOf<Ok>()); // NotFound object or derivative of NotFound
+            Assert.That(result, Is.InstanceOf<ActionResult>()); // example usage
+        }
+    }
+}
