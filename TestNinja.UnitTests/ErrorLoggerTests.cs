689873bfa851f0ab65b098b513e442a7f8ab801f
Will Reichl <William.Reichl@dcsg.com>
2019-01-01 21:38:52 -0500

    S3L32 - Testing Void Methods S3L33 - Testing Methods that Throw an Exception

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty ()
+        {
+            _logger.Log("a");
+
+            Assert.That(_logger.LastError, Is.EqualTo("a"));               
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowsNullException(string error)
+        {
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+
+            // This test actually doesn't compile. I don't want to spend any more time figuring out why.
+            //Assert.That(() => _logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>);
+        }
+    }
+}
