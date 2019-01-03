5504c17a10b4f40941c2202e02cb52c3d91b144a
Will Reichl <William.Reichl@dcsg.com>
2019-01-02 19:17:37 -0500

    S4L40 - FizzBuzz Exercise Solution

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(0, "FizzBuzz")]
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        public void GetOutput_WhenCalled_GetsExpectedResult(int num, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(num);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
