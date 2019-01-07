1d4e0abd15d29e058c29caa3a97060530a49efd9
Will Reichl <William.Reichl@dcsg.com>
2019-01-07 08:44:39 -0500

    S6L66 - Delete Employee exercise - Step 1 - Extract External Depency to its own Class

--- TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/Mocking/EmployeeController.cs
@@ -4,18 +4,15 @@ namespace TestNinja.Mocking
 {
     public class EmployeeController
     {
-        private EmployeeContext _db;
-
         public EmployeeController()
         {
-            _db = new EmployeeContext();
+            
         }
 
         public ActionResult DeleteEmployee(int id)
         {
-            var employee = _db.Employees.Find(id);
-            _db.Employees.Remove(employee);
-            _db.SaveChanges();
+            var employeeStorage = new EmployeeStorage();
+            employeeStorage.DeleteEmployee(id);
             return RedirectToAction("Employees");
         }
 
