988dc32a077da3ed9de86e1809b12dc15115a676
Will Reichl <William.Reichl@dcsg.com>
2019-01-07 13:32:47 -0500

    S7 - BookingHelper Project - Added helper methods and added repo setup to SetUp method.

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -13,39 +13,57 @@ namespace TestNinja.UnitTests.Mocking
     public class BookingHelperTests
     {
         private Mock<IBookingRepository> _mockBookingRepo;
+        private Booking _existingBooking;
 
         [SetUp]
         public void SetUp()
         {
             _mockBookingRepo = new Mock<IBookingRepository>();
+            _existingBooking = new Booking
+            {
+                Id = 2,
+                ArrivalDate = ArriveOn(2017, 1, 15), // 15-JAN-2017 2:00PM
+                DepartureDate = DepartOn(2017, 1, 20),
+                Reference = "a"
+            };
+            _mockBookingRepo.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+            {
+                _existingBooking
+            }.AsQueryable()); //AsQueryable is important here, cannot implicitly convert List to IQueryable
         }
 
         [Test]
         public void OverlappingBookingsExist_BookingStartsAndFinishesBeforeAnExistingBooking_ReturnsEmptyString()
         {
-            _mockBookingRepo.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
-            {
-                new Booking
-                {
-                    Id = 2,
-                    ArrivalDate = new DateTime(2017, 1, 15, 14, 0, 0), // 15-JAN-2017 2:00PM
-                    DepartureDate = new DateTime(2017, 1, 20, 10, 0, 0),
-                    Reference = "a"
-                }
-            }.AsQueryable()); //AsQueryable is important here, cannot implicitly convert List to IQueryable
-
             var currentBooking = new Booking
             {
                 Id = 1,
-                ArrivalDate = new DateTime(2017, 1, 10, 14, 0, 0), // 15-JAN-2017 2:00PM
-                DepartureDate = new DateTime(2017, 1, 14, 10, 0, 0),
+                ArrivalDate = Before(_existingBooking.ArrivalDate, daysBefore: 2), // 15-JAN-2017 2:00PM
+                DepartureDate = Before(_existingBooking.ArrivalDate, daysBefore: 1),
                 Reference = "a"
             };
-
             var result = BookingHelper.OverlappingBookingsExist(currentBooking, _mockBookingRepo.Object);
-
             Assert.That(result, Is.EqualTo(""));
+        }
 
+        private DateTime Before(DateTime dateTime, int daysBefore)
+        {
+            return dateTime.AddDays(daysBefore * -1);
+        }
+
+        private DateTime After(DateTime dateTime)
+        {
+            return dateTime.AddDays(1);
+        }
+
+        private DateTime ArriveOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 14, 0, 0);
+        }
+
+        private DateTime DepartOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 10, 0, 0);
         }
     }
 }
